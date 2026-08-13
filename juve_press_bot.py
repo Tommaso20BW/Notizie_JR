41631fe2b5e495f48dd400a83b651ad3e5daf4b9
Tommaso <tommasino2o@gmail.com>
2026-08-13 07:53:01 +0200

    Update juve_press_bot.py

--- juve_press_bot.py
+++ juve_press_bot.py
@@ -129,7 +129,7 @@ YOUTUBE_CHANNELS = (
         "channel_url": "https://www.youtube.com/@Juventus",
     },
     {
-        "source": "YouTube - Fabrizio Romano",
+        "source": "YouTube - Fabrizio Romano in Italiano",
         "channel_id": "UC7pT9g1-oKwVgbpipZODvBA",
         "channel_url": "https://www.youtube.com/@FabrizioRomanoItaliano",
     },
